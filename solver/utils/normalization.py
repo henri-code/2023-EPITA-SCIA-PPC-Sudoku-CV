f8ce0d5a8033214b59992d02fa01bdd4303c4cd9
Francois Soulier <francois.soulier@epita.fr>
2023-03-22 21:39:00 +0100

    solver: Finished implemented first model prototype with 100% accuracy

--- solver/utils/normalization.py
+++ solver/utils/normalization.py
@@ -1,12 +1,14 @@
 import numpy as np
 
-def normalize(a: np.ndarray) -> np.ndarray:
+def normalize(a: np.ndarray, is_test: bool = False) -> np.ndarray:
     """
     Normalize the input array to the range [-0.5, 0.5]
     :param a: Input array
     :return: Normalized array
     """
-    return (a / 9) -0.5
+    if is_test:
+        return a - 1
+    return (a / 9) - 0.5
 
 def denormalize(a: np.ndarray) -> np.ndarray:
     """
